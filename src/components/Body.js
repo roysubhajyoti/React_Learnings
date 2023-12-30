67ce842eea334aae19c9bd89c2a478bb3d3221b2
roysubhajyoti <roysubbhajyoti272@gmail.com>
2023-12-31 01:15:04 +0530

    search bar

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,32 @@
+import { useState } from "react";
 import { resturantList } from "../constants";
 import ResturantCard from "./ResturantCard";
 const Body = () => {
+  const [searchInput, setSearchInput] = useState("KFC");
   return (
-    <div className="body">
-      <div className="search">Search Bar</div>
-      <div className="res-container">
-        {resturantList.map((resturant) => {
-          return <ResturantCard {...resturant.info} key={resturant.info.id} />;
-        })}
+    <>
+      <div className="search-conatiner">
+        <input
+          type="text"
+          className="search-ip"
+          placeholder="search"
+          value={searchInput}
+          onChange={(e) => {
+            setSearchInput(e.target.value);
+          }}
+        />
+        <button className="search-btn">Search</button>
       </div>
-    </div>
+      <div className="cardcontainer">
+        <div className="res-container">
+          {resturantList.map((resturant) => {
+            return (
+              <ResturantCard {...resturant.info} key={resturant.info.id} />
+            );
+          })}
+        </div>
+      </div>
+    </>
   );
 };
 
