265f534f296e8054c7e9a7965132a1493608d2fa
roysubhajyoti <roysubbhajyoti272@gmail.com>
2023-12-31 16:15:02 +0530

    search bar

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,8 +1,18 @@
 import { useState } from "react";
 import { resturantList } from "../constants";
 import ResturantCard from "./ResturantCard";
+
+function filterData(searchInput, resturants) {
+  return resturants.filter((resturant) =>
+    resturant.info.name.includes(searchInput)
+  );
+}
+
 const Body = () => {
-  const [searchInput, setSearchInput] = useState("KFC");
+  const [resturants, setResturants] = useState(resturantList);
+  const [searchInput, setSearchInput] = useState("");
+  //const searchClicked =false;
+  // const [searchClicked, setSearchClicked] = useState("false");
   return (
     <>
       <div className="search-conatiner">
@@ -13,13 +23,26 @@ const Body = () => {
           value={searchInput}
           onChange={(e) => {
             setSearchInput(e.target.value);
+            if (e.target.value === "") {
+              setResturants(resturants);
+            }
           }}
         />
-        <button className="search-btn">Search</button>
+        <button
+          className="search-btn"
+          onClick={() => {
+            //filter the data according to the input
+            let data = filterData(searchInput, resturants);
+            // update the data
+            setResturants(data);
+          }}
+        >
+          Search
+        </button>
       </div>
       <div className="cardcontainer">
         <div className="res-container">
-          {resturantList.map((resturant) => {
+          {resturants.map((resturant) => {
             return (
               <ResturantCard {...resturant.info} key={resturant.info.id} />
             );
